d4994129b36bd310e71ea425b7669986364b7290
Cameron <cameronbromley8@gmail.com>
2021-10-26 16:46:01 +0100

    create route fixed

--- api/models/habitmodels.js
+++ api/models/habitmodels.js
@@ -7,7 +7,7 @@ module.exports = class Habit {
         this.habitName = data.habitName;
         this.quantity = data.quantity;
         this.history = data.history;
-        this.createdDate = data.createdDate;
+        this.created_date = data.created_date;
         this.goodHabit = data.goodHabit;
         this.units = data.units;
         this.days = data.days;
@@ -20,7 +20,7 @@ module.exports = class Habit {
                 const db = await init() 
                 let data = await db.collection("habits").find().toArray()
                 let habits = data.map(d => new Habit({ ...d}))
-                console.log(habits)
+                
                 resolve(habits);
             } catch (err) {
                 reject("Error retrieving habits.")
@@ -39,17 +39,32 @@ module.exports = class Habit {
             }
         })
     }
-    static create(userID, habitName, goodHabit, units="completions", quantity, days){
+    static create(data){
         return new Promise (async (resolve, reject) => {
             try {
+                let {userID, habitName, goodHabit, units="completions", quantity, days} = data
+                
                 const db = await init();
-                created_date = new Date()
-                console.log("the date is" , created_date)
-                let newHabit = db.collection('habits').insertOne({"userID":userID, "habitName":habitName, "goodHabit":goodHabit, "units":units, "quantity":quantity, "days":days, "created_date":created_date, "history":[] })
+                const created_date = Date();
+                console.log(created_date)
+                let newHabit = await db.collection('habits').insertOne({"userID":userID, "habitName":habitName, "goodHabit":goodHabit, "units":units, "created_date":created_date, "quantity":quantity, "days":days, "history":[] })
+                console.log(newHabit)
                 resolve (newHabit);
             } catch (err) {
                 reject('Error creating habit');
             }
         });
+    }
+    static getByHabit_Id(habitID){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init()
+                
+                let data = await db.collection("habits").find({_id:habitID}).toArray()
+                resolve(data);
+            } catch (err) {
+                reject("Error retrieving ID.")
+            }
+        })
     }
 };
