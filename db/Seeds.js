841c82ffe4d5779c1cdabc8eaf72d2090034a8c8
Max Mackie <maxmackie13@gmail.com>
2021-10-26 11:31:14 +0100

    added habit information to seed data

--- db/Seeds.js
+++ db/Seeds.js
@@ -3,7 +3,37 @@ db = connect("localhost:27017/habit")
 db.habits.drop()
 
 db.habits.insert([
-    {name: "water", quantity: 7, userID: "6176870f585134fd7e4d2454"}
+    {
+        discreteHabits: {
+            {water: {
+                completions_per_day: 5, 
+                days:{mon:true, tues:true, wed:true, thur:true, fri:true, sat:true, sun:true}
+                created_date: "Tue Oct 26 2021 11:13:54 GMT+0100 (British Summer Time)",
+                history: [],
+                },
+            {run: {
+                completions_per_day: 1}, 
+                days:{mon:true, tues:true, wed:true, thur:true, fri:true, sat:true, sun:true},
+                created_date: "Tue Oct 26 2021 11:13:54 GMT+0100 (British Summer Time)",
+                history: [],
+                },
+            {musicPractice: {
+                completions_per_day: 5, 
+                days:{mon:true, tues:true, wed:false, thur:true, fri:false, sat:true, sun:false}
+                created_date: "Tue Oct 26 2021 11:13:54 GMT+0100 (British Summer Time)",
+                history: [],
+                },
+            }
+        },
+        badHabits: {
+            smoking: {
+                created_date: "Tue Oct 26 2021 11:13:54 GMT+0100 (British Summer Time)",
+                history: []
+            }
+        }
+    
+        userID: "6176870f585134fd7e4d2454"
+    }
 ])
 
 db.users.drop()
